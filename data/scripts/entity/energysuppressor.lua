2d8dd8fc6d0955db947924133146f2168b659e59
Rinart73 <rinart73@gmail.com>
2019-09-05 14:30:36 +0200

    1.1.8 * Updated to 0.26 * Improved compatibility

--- data/scripts/entity/energysuppressor.lua
+++ data/scripts/entity/energysuppressor.lua
@@ -1,29 +1,32 @@
 include("faction")
 include("stringutility")
 
+local improvedEnergySuppressor_initialize, improvedEnergySuppressor_initUI, improvedEnergySuppressor_onSync, improvedEnergySuppressor_secure, improvedEnergySuppressor_restore -- extended functions
+local improvedEnergySuppressor_sendMailCheckBox -- client UI
+local improvedEnergySuppressor_translatedText -- server
+
+
 if onClient() then -- CLIENT
 
 
-local sendMailCheckBox
-
-local old_initialize = EnergySuppressor.initialize
-function EnergySuppressor.initialize()
-    if old_initialize then old_initialize() end
+improvedEnergySuppressor_initialize = EnergySuppressor.initialize
+function EnergySuppressor.initialize(...)
+    improvedEnergySuppressor_initialize(...)
 
     if checkEntityInteractionPermissions(Entity(), AlliancePrivilege.ManageShips) then
-        invokeServerFunction("setTranslatedMessage", "Energy Signature Suppressor"%_t,
+        invokeServerFunction("improvedEnergySuppressor_setTranslatedMessage", "Energy Signature Suppressor"%_t,
           [[Your energy signature suppressor in sector \s(%s:%s) burnt out!]]%_t,
           [[Your alliance energy signature suppressor in sector \s(%s:%s) burnt out!]]%_t)
     end
 end
 
-function EnergySuppressor.interactionPossible(playerIndex, option)
+function EnergySuppressor.interactionPossible(playerIndex, option) -- overridden
     return checkEntityInteractionPermissions(Entity(), AlliancePrivilege.ManageShips)
 end
 
-local old_initUI = EnergySuppressor.initUI
+improvedEnergySuppressor_initUI = EnergySuppressor.initUI
 function EnergySuppressor.initUI()
-    if old_initUI then old_initUI() end
+    if improvedEnergySuppressor_initUI then improvedEnergySuppressor_initUI() end
 
     local res = getResolution()
     local size = vec2(450, 50)
@@ -35,38 +38,38 @@ function EnergySuppressor.initUI()
     window.moveable = 1
     menu:registerWindow(window, "Settings"%_t)
 
-    sendMailCheckBox = window:createCheckBox(Rect(10, 10, window.width - 10, 30), "Send a mail when suppressor will burn out"%_t, "onSendMailChecked")
-    sendMailCheckBox.captionLeft = false
-    sendMailCheckBox.fontSize = 13
-    sendMailCheckBox:setCheckedNoCallback(EnergySuppressor.data.sendmail)
+    improvedEnergySuppressor_sendMailCheckBox = window:createCheckBox(Rect(10, 10, window.width - 10, 30), "Send a mail when suppressor will burn out"%_t, "improvedEnergySuppressor_onSendMailChecked")
+    improvedEnergySuppressor_sendMailCheckBox.captionLeft = false
+    improvedEnergySuppressor_sendMailCheckBox.fontSize = 13
+    improvedEnergySuppressor_sendMailCheckBox:setCheckedNoCallback(EnergySuppressor.data.sendmail)
 end
 
-local old_onSync = EnergySuppressor.onSync
-function EnergySuppressor.onSync()
-    if old_onSync then old_onSync() end
-    if sendMailCheckBox then
-        sendMailCheckBox:setCheckedNoCallback(EnergySuppressor.data.sendmail)
+improvedEnergySuppressor_onSync = EnergySuppressor.onSync
+function EnergySuppressor.onSync(...)
+    improvedEnergySuppressor_onSync(...)
+
+    if improvedEnergySuppressor_sendMailCheckBox then
+        improvedEnergySuppressor_sendMailCheckBox:setCheckedNoCallback(EnergySuppressor.data.sendmail)
     end
 end
 
-function EnergySuppressor.onSendMailChecked(checkbox, value)
-    invokeServerFunction("onSendMailChecked", nil, value)
+function EnergySuppressor.improvedEnergySuppressor_onSendMailChecked(checkbox, value)
+    invokeServerFunction("improvedEnergySuppressor_onSendMailChecked", nil, value)
 end
 
 
 else -- SERVER
 
 
-local translatedText
-
-function EnergySuppressor.updateServer(timeStep)
+function EnergySuppressor.updateServer(timeStep) -- overridden
     EnergySuppressor.data.time = EnergySuppressor.data.time - timeStep
 
     if EnergySuppressor.data.time <= 0 then
-        local x, y = Sector():getCoordinates()
+        local sector = Sector()
+        local x, y = sector:getCoordinates()
         -- send message
-        if not translatedText then
-            translatedText = {
+        if not improvedEnergySuppressor_translatedText then
+            improvedEnergySuppressor_translatedText = {
               header = "Energy Signature Suppressor"%_t,
               playerMsg = [[Your energy signature suppressor in sector \s(%s:%s) burnt out!]]%_t,
               allianceMsg = [[Your alliance energy signature suppressor in sector \s(%s:%s) burnt out!]]%_t
@@ -74,20 +77,20 @@ function EnergySuppressor.updateServer(timeStep)
         end
         local faction = getParentFaction()
         if faction.isAlliance then
-            faction:sendChatMessage(translatedText.header, ChatMessageType.Warning, translatedText.allianceMsg, x, y)
+            faction:sendChatMessage(improvedEnergySuppressor_translatedText.header, ChatMessageType.Warning, improvedEnergySuppressor_translatedText.allianceMsg, x, y)
         else
-            faction:sendChatMessage(translatedText.header, ChatMessageType.Warning, translatedText.playerMsg, x, y)
+            faction:sendChatMessage(improvedEnergySuppressor_translatedText.header, ChatMessageType.Warning, improvedEnergySuppressor_translatedText.playerMsg, x, y)
         end
         -- send mail
         if EnergySuppressor.data.sendmail then
             local mail = Mail()
-            mail.header = translatedText.header
-            mail.sender = translatedText.header
+            mail.header = improvedEnergySuppressor_translatedText.header
+            mail.sender = improvedEnergySuppressor_translatedText.header
             if faction.isPlayer then
-                mail.text = string.format(string.gsub(translatedText.playerMsg, [[\s]], ''), x, y)
+                mail.text = string.format(string.gsub(improvedEnergySuppressor_translatedText.playerMsg, [[\s]], ''), x, y)
                 Player(faction.index):addMail(mail)
             elseif faction.isAlliance then -- send message to every Alliance member that can manage ships
-                mail.text = string.format(string.gsub(translatedText.allianceMsg, [[\s]], ''), x, y)
+                mail.text = string.format(string.gsub(improvedEnergySuppressor_translatedText.allianceMsg, [[\s]], ''), x, y)
                 local alliance = Alliance(faction.index)
                 for index, _ in pairs({alliance:getMembers()}) do
                     if alliance:hasPrivilege(index, AlliancePrivilege.ManageShips) then
@@ -101,34 +104,37 @@ function EnergySuppressor.updateServer(timeStep)
         local position = entity.position
         local plan = entity:getMovePlan()
         -- remove energy suppressor
-        Sector():deleteEntity(entity)
+        sector:deleteEntity(entity)
         -- create a wreckage in its place
-        Sector():createWreckage(plan, position)
+        sector:createWreckage(plan, position)
     end
 end
 
-local old_secure = EnergySuppressor.secure
+improvedEnergySuppressor_secure = EnergySuppressor.secure
 function EnergySuppressor.secure()
-    local data = old_secure()
-    data.translatedText = translatedText
+    local data = improvedEnergySuppressor_secure()
+
+    data.translatedText = improvedEnergySuppressor_translatedText
+
     return data
 end
 
-local old_restore = EnergySuppressor.restore
+improvedEnergySuppressor_restore = EnergySuppressor.restore
 function EnergySuppressor.restore(data)
-    translatedText = data.translatedText
+    improvedEnergySuppressor_translatedText = data.translatedText
     data.translatedText = nil -- no need to spam this to clients
-    old_restore(data)
+
+    improvedEnergySuppressor_restore(data)
 end
 
-function EnergySuppressor.setTranslatedMessage(header, playerMsg, allianceMsg)
-    if not checkEntityInteractionPermissions(Entity(), AlliancePrivilege.ManageShips) or translatedText then return end
+function EnergySuppressor.improvedEnergySuppressor_setTranslatedMessage(header, playerMsg, allianceMsg)
+    if improvedEnergySuppressor_translatedText or not checkEntityInteractionPermissions(Entity(), AlliancePrivilege.ManageShips) then return end
 
-    translatedText = { header = header, playerMsg = playerMsg, allianceMsg = allianceMsg }
+    improvedEnergySuppressor_translatedText = { header = header, playerMsg = playerMsg, allianceMsg = allianceMsg }
 end
-callable(EnergySuppressor, "setTranslatedMessage")
+callable(EnergySuppressor, "improvedEnergySuppressor_setTranslatedMessage")
 
-function EnergySuppressor.onSendMailChecked(checkbox, value)
+function EnergySuppressor.improvedEnergySuppressor_onSendMailChecked(checkbox, value)
     if not checkEntityInteractionPermissions(Entity(), AlliancePrivilege.ManageShips) then
         return
     end
@@ -136,7 +142,7 @@ function EnergySuppressor.onSendMailChecked(checkbox, value)
     EnergySuppressor.data.sendmail = value
     EnergySuppressor.sync()
 end
-callable(EnergySuppressor, "onSendMailChecked")
+callable(EnergySuppressor, "improvedEnergySuppressor_onSendMailChecked")
 
 
 end
